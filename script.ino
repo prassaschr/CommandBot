25df95a8db7c013a1b974be8f43c7f75fbcd03ab
prassaschr <prassaschr@gmail.com>
2018-01-12 21:30:50 +0200

    Update FOTA

--- script.ino
+++ script.ino
@@ -28,12 +28,17 @@
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <UniversalTelegramBot.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 // Wifi connection configuration data
 //char ssid[] = "XXXXXX";     // your network SSID (name)
 //char password[] = "XXXXXXXXXX"; // your network key
+
+const int FW_VERSION = 1244;
+const char* fwUrlBase = "https://your_site.com/fota/";
+
 
 
 volatile bool telegramButtonPressedFlag = false;
@@ -51,7 +56,72 @@ void saveConfigCallback () {
   shouldSaveConfig = true;
 }
 
-//#define TELEGRAM_BUTTON_PIN D5 //14
+void checkForUpdates() {
+  String mac = getMAC();
+  String fwURL = String( fwUrlBase );
+  fwURL.concat( mac );
+  String fwVersionURL = fwURL;
+  fwVersionURL.concat( ".version" );
+
+  Serial.println( "Checking for firmware updates." );
+  Serial.print( "MAC address: " );
+  Serial.println( mac );
+  Serial.print( "Firmware version URL: " );
+  Serial.println( fwVersionURL );
+
+  HTTPClient httpClient;
+  httpClient.begin( fwVersionURL );
+  int httpCode = httpClient.GET();
+  if( httpCode == 200 ) {
+    String newFWVersion = httpClient.getString();
+
+    Serial.print( "Current firmware version: " );
+    Serial.println( FW_VERSION );
+    Serial.print( "Available firmware version: " );
+    Serial.println( newFWVersion );
+
+    int newVersion = newFWVersion.toInt();
+
+    if( newVersion > FW_VERSION ) {
+      Serial.println( "Preparing to update" );
+
+      String fwImageURL = fwURL;
+      fwImageURL.concat( ".bin" );
+      t_httpUpdate_return ret = ESPhttpUpdate.update( fwImageURL );
+
+      switch(ret) {
+        case HTTP_UPDATE_FAILED:
+          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+          break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+          Serial.println("HTTP_UPDATE_NO_UPDATES");
+          break;
+      }
+    }
+    else {
+      Serial.println( "Already on latest version" );
+    }
+  }
+  else {
+    Serial.print( "Firmware version check failed, got HTTP response code " );
+    Serial.println( httpCode );
+  }
+  httpClient.end();
+}
+
+String getMAC()
+{
+  uint8_t mac[6];
+  char result[14];
+
+ snprintf( result, sizeof( result ), "%02x%02x%02x%02x%02x%02x", mac[ 0 ], mac[ 1 ], mac[ 2 ], mac[ 3 ], mac[ 4 ], mac[ 5 ] );
+
+  return String( result );
+}
+  
+  
+  //#define TELEGRAM_BUTTON_PIN D5 //14
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
